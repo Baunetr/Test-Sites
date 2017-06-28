1d7c4103d0d9a28c55d0ca5d22301a8522ef442d
Andrew Kothen <baunetr@gmail.com>
2017-06-27 20:44:00 -0700

    Complete Milemarker 2

--- ep-support-portal/src/app/app.module.ts
+++ ep-support-portal/src/app/app.module.ts
@@ -3,21 +3,17 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms'; // NgModule Live here
 import { DataTablesModule } from 'angular-datatables';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { UsersTableComponent } from '././tables/users-table/users-table.component';
 import { CompanyComponent } from './pages/company/company.component';
 import { ReportsTableComponent } from './tables/reports-table/reports-table.component';
 import { PageNotFoundComponent } from './not-found.component';
 
 
-const appRoutes: Routes = [
-  { path: 'users', component: UsersTableComponent },
-  { path: 'reports', component: ReportsTableComponent },
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
-]
+
 
 
 @NgModule({
@@ -33,7 +29,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule, // import the Forms Module before binding with [(NgModule)]
     DataTablesModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
