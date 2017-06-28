9357890950aec0cbd3520120587c3babb833a5c4
Andrew Kothen <baunetr@gmail.com>
2017-06-27 20:27:32 -0700

    Complete Milestone 1
    moving on

--- ep-support-portal/src/app/app.module.ts
+++ ep-support-portal/src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms'; // NgModule Live here
 import { DataTablesModule } from 'angular-datatables';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UsersTableComponent } from '././tables/users-table/users-table.component';
 import { CompanyComponent } from './pages/company/company.component';
 import { ReportsTableComponent } from './tables/reports-table/reports-table.component';
+import { PageNotFoundComponent } from './not-found.component';
 
+
+const appRoutes: Routes = [
+  { path: 'users', component: UsersTableComponent },
+  { path: 'reports', component: ReportsTableComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+]
 
 
 @NgModule({
@@ -16,13 +25,15 @@ import { ReportsTableComponent } from './tables/reports-table/reports-table.comp
     AppComponent,
     UsersTableComponent,
     CompanyComponent,
-    ReportsTableComponent
+    ReportsTableComponent,
+    PageNotFoundComponent
   ],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
     FormsModule, // import the Forms Module before binding with [(NgModule)]
-    DataTablesModule
+    DataTablesModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
