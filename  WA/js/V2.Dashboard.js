fea97a733395f337a63e490f87e8c73f771c6f04
Andrew Kothen <baunetr@gmail.com>
2018-05-10 17:13:59 -0700

    update

---
+++  WA/js/V2.Dashboard.js
@@ -0,0 +1,27 @@
+$(window).scroll(function() {
+    if ( $(window).scrollTop() >= 28 ) {
+        $('#nav-1').attr('class', 'd-none');
+
+        $('#nav-2').attr('class', 'navbar navbar-expand-md fixed-top col-md-12');
+
+    } else {
+        $('#nav-1').attr('class', 'navbar navbar-expand-md fixed-top col-md-12');
+
+        $('#nav-2').attr('class', 'd-none');
+
+    }
+});
+
+$(window).scroll(function() {
+    if ( $(window).scrollTop() >= 30 ) {
+        $('#nav-1').css('opacity', '0');
+
+        $('#nav-2').css('opacity', '1.0');
+
+    } else {
+        $('#nav-1').css('opacity', '1.0');
+
+        $('#nav-2').css('opacity', '0');
+
+    }
+});
